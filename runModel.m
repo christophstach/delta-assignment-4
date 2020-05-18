ddbcf61e9a69fa2baadcd5f79b897f86cc16617f
Christoph <Stach>
2020-05-18 13:47:46 +0800

    wip

---
+++ runModel.m
@@ -0,0 +1,73 @@
+load('models/quadraticSVM.mat');
+
+varNames = {
+    'VarName1'
+    'VarName2'
+    'VarName3'
+    'VarName4'
+    'VarName5'
+    'VarName6'
+    'VarName7'
+    'VarName8'
+    'VarName9'
+    'VarName10'
+    'VarName11'
+    'VarName12'
+    'VarName13'
+    'VarName14'
+    'VarName15'
+    'VarName16'
+    'VarName17'
+    'VarName18'
+    'VarName19'
+    'VarName20'
+    'VarName21'
+    'VarName22'
+    'VarName23'
+    'VarName24'
+    'VarName25'
+    'VarName26'
+    'VarName27'
+    'VarName28'
+    'VarName29'
+    'VarName30'
+    'VarName31'
+    'VarName32'
+    'VarName33'
+    'VarName34'
+    'VarName35'
+    'VarName36'
+    'VarName37'
+    'VarName38'
+    'VarName39'
+    'VarName40'
+    'VarName41'
+    'VarName42'
+    'VarName43'
+    'VarName44'
+    'VarName45'
+    'VarName46'
+    'VarName47'
+    'VarName48'
+    'VarName49'
+    'VarName50'
+    'VarName51'
+    'VarName52'
+    'VarName53'
+    'VarName54'
+    'VarName55'
+    'VarName56'
+    'VarName57'
+    'VarName58'
+    'VarName59'
+    'VarName60'
+};
+
+
+input = [
+    1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
+];
+
+table = array2table(input, 'VariableNames', varNames);
+prediction = quadraticSVM.predictFcn(table);
+disp(prediction);
